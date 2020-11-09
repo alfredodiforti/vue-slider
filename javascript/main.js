219a210af178daab8bfaa7dfd3aab0d32373d73c
Alfredo Di Forti <adf.palermo@gmail.com>
2020-11-09 17:04:05 +0100

    CSS completato

---
+++ javascript/main.js
@@ -0,0 +1,17 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        indexPhoto: 0,
+        photos: [
+            './img/hamilton.jpg',
+            './img/leclerc.jpg',
+            './img/norris.jpg',
+            './img/verstappen.webp',
+        ]
+    },
+    methods: {
+        nextPhoto()
+
+    }
+
+});
